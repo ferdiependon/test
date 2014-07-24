4e37fb240004a180fbb4165eee4cc606176232c5
Ferdie <f.pendon@klab.com>
2014-07-24 11:10:34 +0800

    Change.

--- test.sql
+++ test.sql
@@ -2,5 +2,6 @@ CREATE TABLE `test` (
   `player_id` int(10) unsigned NOT NULL,
   `num_kill` int(10) unsigned NOT NULL,
   `test` int(10) unsigned NOT NULL,
+  `test2` int(10) unsigned NOT NULL,
   PRIMARY KEY (`player_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
